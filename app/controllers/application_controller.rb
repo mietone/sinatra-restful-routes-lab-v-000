1d50bb1c7c137e03e56c7abd34e5ba6fa6946f6a
Miwa <paintfx@gmail.com>
2018-03-07 21:46:51 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,98 +1,47 @@
-require 'pry'
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+  # register Sinatra::ActiveRecordExtension
+  set :views, Proc.new { File.join(root, "../views/") }
 
-  # CREATE =========================
-
-  get '/recipes/new' do
+  get '/recipes/new' do #loads new form
     erb :new
   end
 
-  post '/recipes' do
-    recipe = Recipe.create(params)
-    redirect "/recipes/#{@recipe.id}"
-  end
-
-  # post '/recipes' do  #creates a recipe
-  #   @recipe = Recipe.create(params)
-  #   redirect to "/recipes/#{@recipe.id}"
-  # end
-
-  # READ =========================
-
-  get '/' do
-    redirect '/recipes'
-  end
-
-  get '/recipes' do
+  get '/recipes' do #loads index page
     @recipes = Recipe.all
     erb :index
   end
 
-
-
-  get '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
+  get '/recipes/:id' do  #loads show page
+    @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
 
-
-#   get '/recipes/:id' do  #loads show page
-#   @recipe = Recipe.find_by_id(params[:id])
-#   erb :show
-# end
-  # # UPDATE =========================
-  #
-  get '/recipes/:id/edit' do
-    @recipe = Recipe.find(params[:id])
+  get '/recipes/:id/edit' do #loads edit form
+    @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
-  # get '/recipes/:id/edit' do #loads edit form
-  #   @recipe = Recipe.find_by_id(params[:id])
-  #   erb :edit
-  # end
 
-  patch '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
+  patch '/recipes/:id' do  #updates a recipe
+    @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    redirect "/recipes/#{@recipe.id}"
+    redirect to "/recipes/#{@recipe.id}"
   end
 
-  # patch '/recipes/:id' do  #updates a recipe
-  #   @recipe = Recipe.find_by_id(params[:id])
-  #   @recipe.name = params[:name]
-  #   @recipe.ingredients = params[:ingredients]
-  #   @recipe.cook_time = params[:cook_time]
-  #   @recipe.save
-  #   redirect to "/recipes/#{@recipe.id}"
-  # end
+  post '/recipes' do  #creates a recipe
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
 
-  #
-  # # DELETE =========================
-  #
-  # delete '/recipes/:id/delete' do
-  #   @recipe = Recipe.find(params[:id])
-  #   @recipe.delete
-  #   redirect '/'
-  # end
-
-
-
-
-
-
-
-
-
-delete '/recipes/:id/delete' do #delete action
-@recipe = Recipe.find_by_id(params[:id])
-@recipe.delete
-redirect to '/recipes'
+  delete '/recipes/:id/delete' do #delete action
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.delete
+  redirect to '/recipes'
 end
+
+
 end
