68074f83e57717bdb6051b47cc433f09cb0afef4
Miwa <paintfx@gmail.com>
2018-03-07 21:25:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    erb :show
+    redirect '/recipes/#{@recipe.id}'
   end
 
   # DELETE =========================
